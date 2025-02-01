6038b71d517ff493ad966a4b6976cbc7c4c04412
code-help007 <kuldip.chauhan@itpl.io>
2025-02-02 00:12:37 +0530

    Cors and other frontend changes

--- ecom-catalog-react/src/App.jsx
+++ ecom-catalog-react/src/App.jsx
@@ -1,34 +1,88 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { useEffect } from 'react';
+import ProductList from './ProductList';
+import CategoryFilter from './CategoryFilter';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [sortOrder, setSortOrder] = useState("asc");
+
+  useEffect(() => {
+    fetch('http://localhost:8080/api/products')
+    .then(response => response.json())
+    .then(data => setProducts(data));
+
+    fetch('http://localhost:8080/api/categories')
+    .then(response => response.json())
+    .then(data => setCategories(data));
+  }, []);
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSortChange = (event) => {
+    setSortOrder(event.target.value);
+  };
+
+  const handleCategorySelect = (categoryId) => {
+    setSelectedCategory(categoryId ? Number(categoryId) : null);
+  };
+
+  const filteredProducts = products
+              .filter(product => {
+                return (
+                  (selectedCategory ? product.category.id === selectedCategory : true)
+                  &&
+                  (product.name.toLowerCase().includes(searchTerm.toLowerCase()))
+                )
+              })
+              .sort((a, b) => {
+                if (sortOrder === "asc") {
+                  return a.price - b.price;
+                } else {
+                  return b.price - a.price;
+                }
+              });
 
   return (
-    <>
+    <div className='container'>
+      <h1 className='my-4'>Product Catalog</h1>
+      <div className='row align-items-center mb-4'>
+        <div className='col-md-3 col-sm-12 mb-2'>
+          <CategoryFilter categories={categories} onSelect={handleCategorySelect}/>
+        </div>
+        <div className='col-md-5 col-sm-12 mb-2'>
+          <input
+            type='text'
+            name='searchTerm'
+            className='form-control'
+            placeholder='Search for products'
+            onChange={handleSearchChange}
+            />
+        </div>
+        <div className='col-md-4 col-sm-12 mb-2'>
+          <select className="form-control" onChange={handleSortChange}>
+            <option value="asc">Sort by Price: Low to High</option>
+            <option value="desc">Sort by Price: High to Low</option>
+          </select>
+        </div>
+      </div>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {filteredProducts.length ? (
+          // Display products
+          <ProductList products={filteredProducts}/>
+        ) : (
+          <p>No products found</p>
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
--- ecom-catalog-sb/src/main/java/com/ecom/productcatalog/controller/ProductController.java
+++ ecom-catalog-sb/src/main/java/com/ecom/productcatalog/controller/ProductController.java
@@ -4,16 +4,14 @@ import com.ecom.productcatalog.model.Product;
 import com.ecom.productcatalog.service.ProductService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/products")
+@CrossOrigin("*")
 public class ProductController {
 
     private final ProductService productService;
